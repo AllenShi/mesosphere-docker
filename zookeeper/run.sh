162a814236f2b48bda7e1ee31ea0ba3ca9c08583
Allen Shi <jinliangshi@gmail.com>
2016-07-20 16:51:29 +0800

    Change apt source and data dir for zookeeper

--- zookeeper/run.sh
+++ zookeeper/run.sh
@@ -3,20 +3,15 @@
 ZOO_CFG="/opt/zookeeper/conf/zoo.cfg"
 
 # Output server ID
+echo "zk data dir: ${ZK_DATA_DIR}"
 echo "server id (myid): ${SERVER_ID}"
-echo "${SERVER_ID}" > /tmp/zookeeper/myid
+echo "${SERVER_ID}" > ${ZK_DATA_DIR}/myid
 
 # Add additional ZooKeeper servers into the zoo.cfg file
+echo "dataDir=${ZK_DATA_DIR}" >> ${ZOO_CFG}
 echo "${ADDITIONAL_ZOOKEEPER_1}" >> ${ZOO_CFG}
 echo "${ADDITIONAL_ZOOKEEPER_2}" >> ${ZOO_CFG}
 echo "${ADDITIONAL_ZOOKEEPER_3}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_4}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_5}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_6}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_7}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_8}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_9}" >> ${ZOO_CFG}
-echo "${ADDITIONAL_ZOOKEEPER_10}" >> ${ZOO_CFG}
 
 # Start Zookeeper
 /opt/zookeeper/bin/zkServer.sh start-foreground
